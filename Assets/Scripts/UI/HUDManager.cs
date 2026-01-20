2e1df8ff473e53819e16d9c700e26d3b576060e8
Loke Öberg <77719393+LokeOberg04@users.noreply.github.com>
2026-01-20 11:31:17 +0100

    car asset + hud

--- Assets/Scripts/UI/HUDManager.cs
+++ Assets/Scripts/UI/HUDManager.cs
@@ -1,3 +1,5 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,11 +7,17 @@ public class HUD : MonoBehaviour
 {
 
     public RawImage SpeedometerArrow;
+    public TextMeshProUGUI TimeUI;
+    public TextMeshProUGUI RacePositionUI;
+
 
     [Range(0, 1)]
     public float SpeedometerValue;
     public float minDegrees;
     public float maxDegrees;
+
+    // might be moved to raceManager
+    private float Timer = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,8 +28,24 @@ public class HUD : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // might be moved to raceManager
+        Timer += Time.deltaTime;
+        SetTimer(Timer);
+        SetRacePosition(1 + (int)Timer % 12);
 
         SpeedometerArrow.transform.localEulerAngles = new Vector3(0, 0, Mathf.Lerp(minDegrees, maxDegrees, SpeedometerValue));
 
     }
+
+    public void SetTimer(float inTimer)
+    {
+        TimeSpan time = TimeSpan.FromSeconds((double)inTimer);
+        TimeUI.text = time.ToString(@"mm\:ss\:fff");
+    }
+
+    public void SetRacePosition(int pos)
+    {
+        RacePositionUI.text = "#"+pos.ToString();
+    }
+
 }
